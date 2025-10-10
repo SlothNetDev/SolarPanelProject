598c199379ce7cc6751635af152984efe31c463a
ProgrammerNoob <sungshigun@gmail.com>
2025-10-10 09:15:15 +0800

    Initial commit

---
+++ SolarPanelProject/src/MainApp/SolarApp.java
@@ -0,0 +1,11 @@
+package MainApp;
+
+import javax.swing.*;
+
+public class SolarApp extends JFrame {
+
+    public static void  main(String[] args) {
+        new SolarApp().setVisible(true);
+    }
+
+}
