0b9d5c975b3189eb6067218103af401bdfb3ecf2
ProgrammerNoob <sungshigun@gmail.com>
2025-10-10 10:44:45 +0800

    feat: Introduce RoundedBorder for modern UI enhancements in ApplianceListPanel

--- SolarPanelProject/src/UI/ApplianceListPanel.java
+++ SolarPanelProject/src/UI/ApplianceListPanel.java
@@ -338,4 +338,22 @@ public class ApplianceListPanel {
             return this;
         }
     }
+    private static class RoundedBorder extends AbstractBorder {
+        private final Color color;
+        private final int radius;
+        RoundedBorder(Color color, int radius) {
+            this.color = color;
+            this.radius = radius;
+        }
+        @Override
+        public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+            Graphics2D g2d = (Graphics2D) g.create();
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2d.setColor(color);
+            g2d.drawRoundRect(x, y, width - 1, height - 1, radius, radius);
+            g2d.dispose();
+        }
+        @Override
+        public Insets getBorderInsets(Component c) { return new Insets(1, 1, 1, 1); }
+    }
 }
